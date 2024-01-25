d78e624b1a90d9740c844719adf8bf87e76bbc4c
Tritchaphorn BOISSON <tritchaphorn@gmail.com>
2024-01-25 10:52:06 +0100

    Create and style profile page

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,7 +1,72 @@
 import "./profile.scss";
+import FacebookOutlinedIcon from "@mui/icons-material/FacebookOutlined";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import PinterestIcon from "@mui/icons-material/Pinterest";
+import XIcon from "@mui/icons-material/X";
+import PlaceIcon from "@mui/icons-material/Place";
+import LanguageIcon from "@mui/icons-material/Language";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import Posts from "../../components/posts/Posts";
 
 const Profile = () => {
-  return <div className="Profile">Profile</div>;
+  return (
+    <div className="profile">
+      <div className="images">
+        <img
+          src="https://images.pexels.com/photos/1049298/pexels-photo-1049298.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt=""
+          className="cover"
+        />
+        <img
+          src="https://images.pexels.com/photos/3866555/pexels-photo-3866555.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt=""
+          className="profilePic"
+        />
+      </div>
+      <div className="profileContainer">
+        <div className="uInfo">
+          <div className="left">
+            <a href="https://facebook.com">
+              <FacebookOutlinedIcon fontSize="large" />
+            </a>
+            <a href="https://facebook.com">
+              <InstagramIcon fontSize="large" />
+            </a>
+            <a href="https://facebook.com">
+              <XIcon fontSize="large" />
+            </a>
+            <a href="https://facebook.com">
+              <LinkedInIcon fontSize="large" />
+            </a>
+            <a href="https://facebook.com">
+              <PinterestIcon fontSize="large" />
+            </a>
+          </div>
+          <div className="center">
+            <span>Jane Doe</span>
+            <div className="info">
+              <div className="item">
+                <PlaceIcon />
+                <span>France</span>
+              </div>
+              <div className="item">
+                <LanguageIcon />
+                <span>FR</span>
+              </div>
+            </div>
+            <button>Follow</button>
+          </div>
+          <div className="right">
+            <EmailOutlinedIcon />
+            <MoreVertIcon />
+          </div>
+        </div>
+        <Posts />
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
