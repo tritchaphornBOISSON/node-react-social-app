3d37a1a4de5fcd25520e052af80300b716ff5af9
Tritchaphorn BOISSON <tritchaphorn@gmail.com>
2024-02-02 10:02:50 +0100

    Implement user authentification with jwt and cookie-parser

--- back-end/controllers/auth.js
+++ back-end/controllers/auth.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcryptjs";
 import { db } from "../connectDB.js";
+import jwt from "jsonwebtoken";
 
 export const register = (req, res) => {
 
@@ -34,10 +35,36 @@ export const register = (req, res) => {
 }
 
 export const login = (req, res) => {
+    const query = "SELECT * FROM users WHERE username = ?";
+    db.query(query, [req.body.username], (err, data) => {
+        if (err) {
+            return res.status(500).json(err);
+        }
 
+        if (data.length === 0) {
+            return res.status(400).json("User not found.");
+        }
+
+        const checkPassword = bcrypt.compareSync(req.body.password, data[0].password);
+
+        if (!checkPassword) {
+            return res.status(400).json("Wrong password or username!");
+        }
+
+        const token = jwt.sign({ id: data[0].id }, "secretkey");
+
+        const { password, ...others } = data[0];
+
+        res.cookie("accessToken", token, {
+            httpOnly: true,
+        }).status(200).json(others);
+    })
 }
 
 
 export const logout = (req, res) => {
-
+    res.clearCookie("accessToken", {
+        secure: true,
+        sameSite: true,
+    }).status(200).json("User has been logged out.")
 }
--- back-end/index.js
+++ back-end/index.js
@@ -4,13 +4,16 @@ import userRoutes from "./routes/users.js";
 import commentRoutes from "./routes/comments.js";
 import postRoutes from "./routes/posts.js";
 import likeRoutes from "./routes/likes.js";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 const port = 5000;
 
 //middlewares
-
+app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
